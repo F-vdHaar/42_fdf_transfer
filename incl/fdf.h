429758db37ffcc439c73c5912ca8656c4d967714
fvon-der <fvon-der@student.42.fr>
2024-07-03 12:35:14 +0200

    header done, keep to this structure for implementation

--- incl/fdf.h
+++ incl/fdf.h
@@ -6,12 +6,13 @@
 /*   By: fvon-der <fvon-der@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/28 15:19:02 by fvon-der          #+#    #+#             */
-/*   Updated: 2024/06/28 16:46:04 by fvon-der         ###   ########.fr       */
+/*   Updated: 2024/07/03 12:33:54 by fvon-der         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FDF_H
 # define FDF_H
+
 # include <mlx.h>
 # include <math.h>
 # include <stdio.h>
@@ -20,29 +21,85 @@
 # include "libft.h"
 # include "ft_printf.h"
 
-typedef struct s_map_dim
+// Structures
+typedef struct s_map
 {
-	int	width;
-	int	height;
-}			t_map_dim;
+	char	**file_content;
+	int		width;
+	int		height;
+	int		**map;
+	int		z_max;
+	int		z_min;
+}	t_map;
 
-typedef struct s_data
+typedef struct s_camera
 {
-	void	*img;
-	char	*addr;
-	int		bits_per_pixel;
-	int		line_length;
-	int		endian;
-}				t_data;
+	int		zoom;
+	double	x_angle;
+	double	y_angle;
+	double	z_angle;
+	float	z_height;
+	int		x_offset;
+	int		y_offset;
+	int		iso;
+}	t_camera;
 
+typedef struct s_mouse
+{
+	int		button;
+	int		x;
+	int		y;
+	int		prev_x;
+	int		prev_y;
+}	t_mouse;
 
-//--------------------------FDF--------------------------
-//--------------------------FDF UTILS-------------------
-void			fast_mlx_pixel_put(t_data *data, int x, int y, int color);
-//-------------------------MAP UTILS-------------------
-int				**parse_map(int fd, t_map_dim *dimensions);
-void			free_map(int **map, int height);
-//-------------------------TEST UTILS-------------------
-void			print_map(int **map);
+typedef struct s_renderer
+{
+	void		*mlx_ptr;
+	void		*win_ptr;
+	void		*img_ptr;
+	char		*img_data;
+	int			bits_per_pixel;
+	int			line_length;
+	int			endian;
+	t_camera	*camera;
+	t_mouse		*mouse;
+}	t_renderer;
+
+typedef struct s_point
+{
+	int			x;
+	int			y;
+}	t_point;
+
+// Function Prototypes
+// Main
+void	initialize(int argc, char *argv[]);
+void	cleanup(void);
+
+// Map Utilities
+int		**parse_map(const char *filename);
+int		validate_map(char **file_content);
+void	free_map(int **map, int height);
+
+// Rendering
+void	draw_line(t_point start, t_point end, int color);
+void	render_map(t_renderer *renderer, t_map *map);
+void	put_pixel(t_renderer *renderer, int x, int y, int color);
+void	display(t_renderer *renderer);
+
+// Event Handling
+int		handle_keypress(int keycode, void *param);
+int		handle_mouse(int button, t_point location, void *param);
+
+// Matrix Transformations
+void	rotate(t_map *map, float angle);
+void	scale(t_map *map, float factor);
+void	translate(t_map *map, int dx, int dy);
+
+// Utility Functions
+void	fast_mlx_pixel_put(t_renderer *renderer, int x, int y, int color);
+void	print_map(int **map);
+void	log_error(const char *message);
 
 #endif
