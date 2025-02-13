04973249f24524ca3db09fcffa03720258d5969d
fvon-der <fvon-der@student.42heilbronn.de>
2025-02-13 04:33:35 +0100

    WP : BUGHUNT SQUASHME Issue Image Buffer Not Properly Set? Issue: Rendering Function Not Updating the Image? Issue : mlx_put_image_to_window() Missing?

--- src/fdf/fdf.c
+++ src/fdf/fdf.c
@@ -6,7 +6,7 @@
 /*   By: fvon-de <fvon-der@student.42heilbronn.d    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/28 15:21:33 by fvon-der          #+#    #+#             */
-/*   Updated: 2025/02/04 02:51:33 by fvon-de          ###   ########.fr       */
+/*   Updated: 2025/02/13 04:22:21 by fvon-de          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,9 @@ int	main(int argc, char **argv)
 	t_renderer	*renderer;
 	t_map		*map;
 
+	ft_printf("DEBUG: initalize\n");
 	initialize(argc, argv, &renderer, &map);
+	ft_printf("DEBUG: cleanup\n" );
 	cleanup(renderer);
 	return (EXIT_SUCCESS);
 }
@@ -31,21 +33,43 @@ void	initialize(int argc, char *argv[], t_renderer **renderer, t_map **map)
 		ft_printf("Please enter a <map_file> for : %s \n", argv[0]);
 		exit(EXIT_FAILURE);
 	}
-	init_renderer(*renderer);
+
+	ft_printf("DEBUG: init render\n");
+
+	// Allocate memory for renderer (since it's a pointer)
+	*renderer = malloc(sizeof(t_renderer));
+	if (!*renderer)
+	{
+		log_error("Failed to allocate memory for renderer");
+		exit(EXIT_FAILURE);
+	}
+
+	ft_memset(*renderer, 0, sizeof(t_renderer));  // Initialize struct memory
+	init_renderer(*renderer);  // Pass the allocated struct
+
 	if (!*renderer)
 		exit(EXIT_FAILURE);
-	init_map(map, argv[1]);
+
+	ft_printf("DEBUG: init map\n");
+	init_map(map, argv[1]);  // Initialize map
 	if (!*map)
 	{
 		cleanup(*renderer);
 		exit(EXIT_FAILURE);
 	}
-	init_camera(*renderer);
-	init_mouse(*renderer);
+
+	ft_printf("DEBUG: init camera\n");
+	init_camera(*renderer);  // Initialize camera
+	ft_printf("DEBUG: init mouse\n");
+	init_mouse(*renderer);   // Initialize mouse
+	ft_printf("DEBUG: setup event hook\n");
 	setup_event_hooks(*renderer);
+	ft_printf("DEBUG: render initial\n");
 	render_initial(*renderer);
+	ft_printf("DEBUG: mlx loop\n");
 	mlx_loop((*renderer)->mlx.mlx_ptr);
 }
+
 
 static void	render_initial(t_renderer *renderer)
 {
--- src/fdf/renderer.c
+++ src/fdf/renderer.c
@@ -6,11 +6,13 @@
 /*   By: fvon-de <fvon-der@student.42heilbronn.d    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/02 15:10:42 by fvon-de           #+#    #+#             */
-/*   Updated: 2025/02/04 02:01:41 by fvon-de          ###   ########.fr       */
+/*   Updated: 2025/02/13 04:15:03 by fvon-de          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
+
+static void	init_image(t_renderer *renderer);
 
 void	put_pixel(t_renderer *renderer, int x, int y, int color)
 {
@@ -27,6 +29,13 @@ void	put_pixel(t_renderer *renderer, int x, int y, int color)
 
 void	init_renderer(t_renderer *renderer)
 {
+	if (!renderer)
+	{
+		printf("ERROR: Failed to allocate memory for renderer\n");
+		log_error("Error initializing MLX");
+		exit(EXIT_FAILURE);
+	}
+	ft_printf("DEBUG: mlx init\n");
 	renderer->mlx.mlx_ptr = mlx_init();
 	if (!renderer->mlx.mlx_ptr)
 	{
@@ -35,6 +44,7 @@ void	init_renderer(t_renderer *renderer)
 	}
 	renderer->win_width = 800;
 	renderer->win_height = 600;
+	ft_printf("DEBUG: mlx new window\n");
 	renderer->mlx.win_ptr = mlx_new_window(renderer->mlx.mlx_ptr,
 			renderer->win_width, renderer->win_height, "FdF");
 	if (!renderer->mlx.win_ptr)
@@ -42,8 +52,46 @@ void	init_renderer(t_renderer *renderer)
 		log_error("Error creating window");
 		exit(EXIT_FAILURE);
 	}
+	ft_printf("DEBUG: init image\n");
 	init_image(renderer);
 	renderer->is_focused = 1;
+
+	renderer->camera = malloc(sizeof(t_camera));
+	if (!renderer->camera)
+	{
+		log_error("Error allocating memory for camera");
+		exit(EXIT_FAILURE);
+	}
+	ft_memset(renderer->camera, 0, sizeof(t_camera));
+	renderer->camera->zoom = 20;
+	renderer->camera->z_height = 1.0;
+	renderer->camera->x_angle = 0;
+	renderer->camera->y_angle = 0;
+	renderer->camera->z_angle = 0;
+	renderer->camera->iso = 1;
+
+	// Initialize map
+	renderer->map = malloc(sizeof(t_map));
+	if (!renderer->map)
+	{
+		log_error("Error allocating memory for map");
+		exit(EXIT_FAILURE);
+	}
+	ft_memset(renderer->map, 0, sizeof(t_map));
+
+	// Initialize mouse
+	renderer->mouse = malloc(sizeof(t_mouse));
+	if (!renderer->mouse)
+	{
+		log_error("Error allocating memory for mouse");
+		exit(EXIT_FAILURE);
+	}
+	ft_memset(renderer->mouse, 0, sizeof(t_mouse));
+
+	// Initialize other fields
+	renderer->is_focused = 1;
+	renderer->z_buffer = NULL; // Set to NULL initially; allocate later if needed
+	renderer->keymode = 0;
 }
 
 static void	init_image(t_renderer *renderer)
